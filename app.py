a1dde289682e0e90c80eb110656758dfabac122d
runkwell <112172369+runkwell@users.noreply.github.com>
2025-12-14 19:23:11 +0700

    e

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Hello World from a Container on AWS ECS!</h1>'
+    return '<h1>Hello World from a Container on ec2!</h1>'
 
 if __name__ == '__main__':
     # Chạy trên cổng 80, hoặc cổng mà container được cấu hình để lắng nghe
